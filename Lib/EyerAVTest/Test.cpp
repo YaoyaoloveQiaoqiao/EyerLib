6559d0f33d7a15f153e585c29fe93f7e563a829c
yuqiaomiao <yuqiaomiao@sohu-inc.com>
2020-08-19 11:16:42 +0800

    Stream Timebase

--- Lib/EyerAVTest/Test.cpp
+++ Lib/EyerAVTest/Test.cpp
@@ -189,6 +189,52 @@ TEST(Encoder, Encoder){
     */
 }
 
+TEST(EyerAVStreamTimebase, EyerAVStreamTimebaseTest){
+    Eyer::EyerAVReader reader("/Users/lichi/annie/xinxiaomen.mp4");
+    int ret = reader.Open();
+    if(ret){
+        printf("Open Fail ret: %d\n", ret);
+        return;
+    }
+
+    int videoStreamIndex = reader.GetVideoStreamIndex();
+    int audioStreamIndex = reader.GetAudioStreamIndex();
+
+    if(videoStreamIndex < 0){
+        printf("Video Stream is NULL\n");
+    }
+    if(audioStreamIndex < 0){
+        printf("Audio Stream is NULL\n");
+    }
+
+    Eyer::EyerAVStream videoStream;
+    reader.GetStream(videoStream, videoStreamIndex);
+    Eyer::EyerAVStream audioStream;
+    reader.GetStream(audioStream, audioStreamIndex);
+
+    int index = 0;
+    while (1) {
+        Eyer::EyerAVPacket packet;
+        ret = reader.Read(&packet);
+        if (ret) {
+            break;
+        }
+
+        if(packet.GetStreamId() == videoStreamIndex){
+            videoStream.ScalerPacketPTS(packet);
+            double secPts = packet.GetSecPTS();
+            printf("Video Sec PTS: %f, index: %d\n", secPts, index);
+            index++;
+        }
+        if(packet.GetStreamId() == audioStreamIndex){
+            audioStream.ScalerPacketPTS(packet);
+            double secPts = packet.GetSecPTS();
+            printf("Audio Sec PTS: %f, index: %d\n", secPts, index);
+            index++;
+        }
+    }
+}
+
 TEST(EyerAVBitstreamFilter, EyerAVBitstreamFilter){
     Eyer::EyerAVReader reader("/Users/lichi/annie/xinxiaomen.mp4");
     int ret = reader.Open();
@@ -229,20 +275,15 @@ TEST(EyerAVBitstreamFilter, EyerAVBitstreamFilter){
                 break;
             }
 
-            printf("dstLen: %d\n", outPacket.GetSize());
-
-            for(int i=0;i<10;i++){
-                printf("%d ", outPacket.GetDataPtr()[i]);
-            }
-            printf("\n");
+            // printf("dstLen: %d\n", outPacket.GetSize());
             for(int i=0;i<outPacket.GetSize()-4;i++){
                 unsigned char * dataPtr = outPacket.GetDataPtr();
                 if(dataPtr[i] == 0){
                     if(dataPtr[i + 1] == 0){
                         if(dataPtr[i + 2] == 0){
                             if(dataPtr[i + 3] == 1){
-                                printf("Miao!!!!\n");
-                                printf("nalu type: %d\n", dataPtr[i + 4] & 0x1F);
+                                // printf("Miao!!!!\n");
+                                // printf("nalu type: %d\n", dataPtr[i + 4] & 0x1F);
                             }
                         }
                     }
@@ -250,16 +291,10 @@ TEST(EyerAVBitstreamFilter, EyerAVBitstreamFilter){
             }
 
             // printf("nalu type: %d\n", outPacket.GetDataPtr()[4] & 0x1F);
-            printf("\n\n");
+            // printf("\n\n");
 
             fwrite(outPacket.GetDataPtr(), outPacket.GetSize(), 1, file);
         }
-
-
-
-
-
-
     }
 
     fclose(file);
