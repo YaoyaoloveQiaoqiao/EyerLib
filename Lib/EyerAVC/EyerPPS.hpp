0f07a28db64fdfed20a3323a0665a07ced589298
yuqiaomiao <yuqiaomiao@sohu-inc.com>
2020-11-03 11:38:49 +0800

    PPS

--- Lib/EyerAVC/EyerPPS.hpp
+++ Lib/EyerAVC/EyerPPS.hpp
@@ -13,6 +13,35 @@ namespace Eyer
         ~EyerPPS();
 
         virtual int Parse();
+
+    private:
+        int pic_parameter_set_id;
+        int seq_parameter_set_id;
+        int entropy_coding_mode_flag;
+        int pic_order_present_flag;
+        int num_slice_groups_minus1;
+        int slice_group_map_type;
+        int run_length_minus1[8]; // up to num_slice_groups_minus1, which is <= 7 in Baseline and Extended, 0 otheriwse
+        int top_left[8];
+        int bottom_right[8];
+        int slice_group_change_direction_flag;
+        int slice_group_change_rate_minus1;
+        int pic_size_in_map_units_minus1;
+        int slice_group_id[256]; // FIXME what size?
+
+        int num_ref_idx_l0_active_minus1;
+        int num_ref_idx_l1_active_minus1;
+        int weighted_pred_flag;
+        int weighted_bipred_idc;
+        int pic_init_qp_minus26;
+        int pic_init_qs_minus26;
+        int chroma_qp_index_offset;
+        int deblocking_filter_control_present_flag;
+        int constrained_intra_pred_flag;
+        int redundant_pic_cnt_present_flag;
+
+
+        int intlog2(int x);
     };
 }
 
