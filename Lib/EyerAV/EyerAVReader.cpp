d3a66879d7747e70be25715e876eb303e485baf7
yuqiaomiao <yuqiaomiao@sohu-inc.com>
2020-11-09 12:44:13 +0800

    Reader

--- Lib/EyerAV/EyerAVReader.cpp
+++ Lib/EyerAV/EyerAVReader.cpp
@@ -34,26 +34,24 @@ namespace Eyer
         av_register_all();
         avformat_network_init();
 
-
-        int nBufferSize = 1024 * 1024 * 2;
-        unsigned char * pBuffer = new unsigned char[nBufferSize];
-
-        // EyerDASHReader * dashReader = new EyerDASHReader(EyerString("https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd"));
-        // EyerDASHReader * dashReader = new EyerDASHReader(EyerString("http://redknot.cn/DASH/xiaomai_dash.mpd"));
-
-        AVIOContext* pIOCtx = avio_alloc_context(pBuffer, nBufferSize,
-                                                 0,
-                                                 dashReader,
-                                                 read_packet,
-                                                 0,
-                                                 0);
-
-
-
         piml->formatCtx = avformat_alloc_context();
 
-        piml->formatCtx->pb = pIOCtx;
 
+        if(dashReader != nullptr){
+            int nBufferSize = 1024 * 1024 * 2;
+            unsigned char * pBuffer = new unsigned char[nBufferSize];
+
+            // EyerDASHReader * dashReader = new EyerDASHReader(EyerString("https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd"));
+            // EyerDASHReader * dashReader = new EyerDASHReader(EyerString("http://redknot.cn/DASH/xiaomai_dash.mpd"));
+
+            AVIOContext* pIOCtx = avio_alloc_context(pBuffer, nBufferSize,
+                                                     0,
+                                                     dashReader,
+                                                     read_packet,
+                                                     0,
+                                                     0);
+            piml->formatCtx->pb = pIOCtx;
+        }
     }
 
     EyerAVReader::~EyerAVReader()
