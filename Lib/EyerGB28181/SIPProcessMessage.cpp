b430a2d022f75f2c5451a02a62ad7c913b4f7cfa
Redknot <redknotmiaoyuqiao@gmail.com>
2020-11-23 01:39:24 +0800

    Update SIPProcessMessage.cpp

--- Lib/EyerGB28181/SIPProcessMessage.cpp
+++ Lib/EyerGB28181/SIPProcessMessage.cpp
@@ -15,6 +15,17 @@ namespace Eyer
         eXosip_message_send_answer (excontext, je->tid, 200, answer);
 
         // eXosip_call_build_request()
+        osip_message_t * request = NULL;
+        eXosip_message_build_request(excontext, &request,"MESSAGE", "sip:34020000001320000001@3402000000", "sip:34020000002000000001@3402000000", NULL);
+
+        eXosip_message_send_request(excontext, request);request
+
+        if(request != NULL){
+            char * str = NULL;
+            size_t strlen = 0;
+            osip_message_to_str(request, &str, &strlen);
+            printf("%s\n", str);
+        }
 
         return 0;
     }
