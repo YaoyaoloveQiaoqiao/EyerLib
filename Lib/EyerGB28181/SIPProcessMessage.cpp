ca1545f2e6cbbeb2d93e4ac9131c3e4efe08b016
Redknot <redknotmiaoyuqiao@gmail.com>
2020-11-23 01:02:55 +0800

    Update SIPProcessMessage.cpp

--- Lib/EyerGB28181/SIPProcessMessage.cpp
+++ Lib/EyerGB28181/SIPProcessMessage.cpp
@@ -4,9 +4,16 @@ namespace Eyer
 {
     int SIPProcessMessage::Process(struct eXosip_t * excontext, eXosip_event_t * je)
     {
+        osip_body_t * dest = NULL;
+        osip_message_get_body(je->request, 0, &dest);
+        if(dest != NULL){
+            printf("wwwwwwww::::%s\n", dest->body);
+        }
+
         osip_message_t * answer = NULL;
         eXosip_message_build_answer (excontext, je->tid, 200, &answer);
         eXosip_message_send_answer (excontext, je->tid, 200, answer);
+
         return 0;
     }
 }
