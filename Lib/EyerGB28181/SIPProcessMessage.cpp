a74715939d488962c53255728e2a915b58a6e364
Redknot <redknotmiaoyuqiao@gmail.com>
2020-11-26 00:54:19 +0800

    Main Thread

--- Lib/EyerGB28181/SIPProcessMessage.cpp
+++ Lib/EyerGB28181/SIPProcessMessage.cpp
@@ -1,5 +1,7 @@
 #include "SIPProcessMessage.hpp"
 
+#include <string>
+
 namespace Eyer
 {
     int SIPProcessMessage::Process(struct eXosip_t * excontext, eXosip_event_t * je)
@@ -15,11 +17,20 @@ namespace Eyer
         eXosip_message_send_answer (excontext, je->tid, 200, answer);
 
         // eXosip_call_build_request()
-        printf("xxxxx:%s\n", je->textinfo);
 
         osip_message_t * request = NULL;
         eXosip_message_build_request(excontext, &request,"MESSAGE", "sip:34020000001320000001@192.168.2.101:5060", "sip:ServerA@Server", NULL);
-        // osip_message_set_route()
+        // osip_message_set_route(request, "sip:34020000001320000001@192.168.2.101:5060");
+
+        char * catalog = (char *)"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                                 "<Query>\n"
+                                 "<CmdType>DeviceInfo</CmdType>\n"
+                                 "<SN>248</SN>\n"
+                                 "<DeviceID>34020000001320000001</DeviceID>\n"
+                                 "</Query>\r\n";
+        osip_message_set_body(request, catalog, strlen(catalog));
+        osip_message_set_content_type(request, "Application/MANSCDP+xml");
+
         eXosip_message_send_request(excontext, request);
 
         if(request != NULL){
