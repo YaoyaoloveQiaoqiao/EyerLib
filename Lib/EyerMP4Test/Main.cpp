c1a5840c2d6526efb26cc7208bde8abec636bf74
yuqiaomiao <yuqiaomiao@sohu-inc.com>
2020-12-18 19:46:22 +0800

    mp4 test

--- Lib/EyerMP4Test/Main.cpp
+++ Lib/EyerMP4Test/Main.cpp
@@ -1,9 +1,7 @@
 #include <gtest/gtest.h>
 
-// #include "Endian.hpp"
+#include "Endian.hpp"
 #include "Read_MP4Test.hpp"
-
-/*
 #include "Read_fMP4Test.hpp"
 
 #include "FTYPTest.hpp"
@@ -18,7 +16,6 @@
 #include "TREXTest.hpp"
 #include "MFHDTest.hpp"
 #include "TFHDTest.hpp"
- */
 
 int main(int argc,char **argv){
     eyer_log_param(1, 1, 0, 0, 0);
